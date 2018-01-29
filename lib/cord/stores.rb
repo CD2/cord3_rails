c194579126e02dd45565c4d3c215b72179c6f7c0
James Page <james.page@cd2solutions.co.uk>
2018-01-29 13:29:22 +0000

    fix

--- lib/cord/stores.rb
+++ lib/cord/stores.rb
@@ -8,27 +8,28 @@ module Cord
         names.each do |name|
           eval <<-RUBY
             def self.#{name} *values
-              return @#{name} if @#{name}
-              @#{name} = (self == Cord::BaseApi ? {} : superclass.#{name}.deep_dup)
+              unless @#{name}
+                @#{name} = (self == Cord::BaseApi ? {} : superclass.#{name}.deep_dup)
 
-              def @#{name}.add *names, &block
-                names.flatten.each do |name|
-                  self[name] = block || ->(x){ x.send(name) }
+                def @#{name}.add *names, &block
+                  names.flatten.each do |name|
+                    self[name] = block || ->(x){ x.send(name) }
+                  end
                 end
-              end
 
-              def @#{name}.remove *names
-                names.flatten.map { |x| ::Cord::BaseApi.normalize(x) }.each do |name|
-                  delete(name)
+                def @#{name}.remove *names
+                  names.flatten.map { |x| ::Cord::BaseApi.normalize(x) }.each do |name|
+                    delete(name)
+                  end
                 end
-              end
 
-              def @#{name}.[] key
-                super ::Cord::BaseApi.normalize(key)
-              end
+                def @#{name}.[] key
+                  super ::Cord::BaseApi.normalize(key)
+                end
 
-              def @#{name}.[]= key, value
-                super ::Cord::BaseApi.normalize(key), value
+                def @#{name}.[]= key, value
+                  super ::Cord::BaseApi.normalize(key), value
+                end
               end
 
               @#{name}.add values
@@ -48,15 +49,17 @@ module Cord
         names = Array.wrap(names[0]) if names.one?
         names.each do |name|
           eval <<-RUBY
-            def self.#{name}
-              @#{name} ||= (self == Cord::BaseApi ? [] : superclass.#{name}.deep_dup)
+            def self.#{name} *values
+              unless @#{name}
+                @#{name} = (self == Cord::BaseApi ? [] : superclass.#{name}.deep_dup)
 
-              def @#{name}.add *names
-                replace(self | names.flatten.map { |x| ::Cord::BaseApi.normalize(x) })
-              end
+                def @#{name}.add *names
+                  replace(self | names.flatten.map { |x| ::Cord::BaseApi.normalize(x) })
+                end
 
-              def @#{name}.remove *names
-                replace(self - names.flatten.map { |x| ::Cord::BaseApi.normalize(x) })
+                def @#{name}.remove *names
+                  replace(self - names.flatten.map { |x| ::Cord::BaseApi.normalize(x) })
+                end
               end
 
               @#{name}.add values
