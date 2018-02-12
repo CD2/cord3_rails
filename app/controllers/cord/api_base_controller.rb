b68b2ca5b252185dab8a03979dcbbe64ef89aacd
James Page <james.page@cd2solutions.co.uk>
2018-02-12 11:06:32 +0000

    fix?

--- app/controllers/cord/api_base_controller.rb
+++ app/controllers/cord/api_base_controller.rb
@@ -110,7 +110,7 @@ module Cord
       if @cord_response[:_errors].any?
         result << { table: :_errors, _errors: [@cord_response[:_errors]] }
       end
-      JSON.generate result
+      result.to_json
     end
 
     def add_queue_item api, body
